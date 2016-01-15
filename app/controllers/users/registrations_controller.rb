619353bcd9c088f72d553c0ed8692cdae2ce7b25
Luís Alberto <lualsiv@gmail.com>
2016-01-14 23:56:42 -0200

    Correction in the profile update.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,27 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-  before_action :configure_permitted_parameters
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:profile_image, :email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:profile_image, :email, :password, :password_confirmation, :current_password)
+  # Overwrite update_resource to let users to update their user without giving their password
+  def update_resource(resource, params)
+    if current_user.provider == "facebook"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
     end
   end
+
+  # before_action :configure_permitted_parameters
+  #
+  # protected
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) do |u|
+  #     u.permit(:profile_image, :email, :password, :password_confirmation)
+  #   end
+  #   devise_parameter_sanitizer.for(:account_update) do |u|
+  #     u.permit(:profile_image, :email, :password, :password_confirmation, :current_password)
+  #   end
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
   attachment :profile_image
   has_one :address, as: :addressable
 
+  def password_required?
+    super && provider.blank?
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
