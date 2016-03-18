b2376bc759864d7b509eaac3be772f85c7ca6143
Douglas Costa <dodop3000@gmail.com>
2016-03-18 09:17:50 -0300

    mmodularize ajax calls for contributors page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,14 +15,3 @@
 	});
 })();
 
-$.ajax({
-  method: "GET",
-  url: "https://api.github.com/repos/learning-ruby/brasil-sem-zika/contributors",
-  dataType: 'json'
- })
-   .done(function(contri) {
- 	  	$.each(contri, function (index, value) {
- 	  		$("#contributors").append("<a href='"+ value.html_url +"'><img src='" + value.avatar_url + "' title='"+ value.login +"' style='border-radius: 20px; width:100px; padding-bottom: 10px; margin-left: 10px;'> </a>");
- 	});
-});
-
