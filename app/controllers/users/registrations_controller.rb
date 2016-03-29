d54b2734e00e0c1a72e0435f66be4486bc78ebd5
Douglas Macedo <dodop3000@gmail.com>
2016-03-29 13:28:02 -0300

    Add user information, last_name and first_name * Get these information from facebook login - login through js client was updated to get the correct permission - controller allows it * Add fields to User model - `app/models/user.rb`

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,10 +16,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:profile_image, :email, :password, :password_confirmation)
+      u.permit(:profile_image, :email, :first_name, :last_name, :address,
+        :cpf, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:profile_image, :email, :password, :password_confirmation, :current_password)
+      u.permit(:profile_image, :email, :first_name, :last_name, :address,
+        :cpf, :password, :password_confirmation, :current_password)
     end
   end
 end
