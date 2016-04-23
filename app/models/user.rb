c00faf9da9281fef6df0194967802245c42c469c
Douglas Macedo <dodop3000@gmail.com>
2016-04-23 11:14:45 -0300

    user image thumbnail

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,24 @@ class User < ActiveRecord::Base
     super && provider.blank?
   end
 
-  def self.from_omniauth(auth)
-    where(provider: auth['provider'], uid: auth['uid']).first_or_create do |user|
-      user.email = auth['info']['email']
-      user.profile_image = open(URI.parse(auth['info']['image']).to_s)
-      user.first_name = auth['info']['first_name']
-      user.last_name = auth['info']['last_name']
-      user.password = Devise.friendly_token[0, 20]
+  class << self
+    def from_omniauth(auth)
+      where(provider: auth['provider'], uid: auth['uid']).first_or_create do |user|
+        user.email = auth['info']['email']
+        user.profile_image = open(URI.parse(auth['info']['image']).to_s)
+        user.first_name = auth['info']['first_name']
+        user.last_name = auth['info']['last_name']
+        user.password = Devise.friendly_token[0, 20]
+      end
     end
-  end
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
-        user.email = data['email'] if user.email.blank?
+    def new_with_session(params, session)
+      super.tap do |user|
+        if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+          user.email = data['email'] if user.email.blank?
+        end
       end
     end
   end
+
 end
